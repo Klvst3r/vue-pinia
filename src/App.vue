767acd35d9adbe1b8e78578a1f11fb7dc3d47c73
Klvst3r <klvst3r@gmail.com>
2026-01-07 12:03:30 -0600

    2 Definiendo la tienda a utilizar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,5 @@
-<script setup>
-import HelloWorl from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+<script setup></script>
 
-<template>
-  <h1>You did it!</h1>
-  <p>
-    Visit
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to
-    read the documentation
-  </p>
-</template>
+<template></template>
 
 <style scoped></style>
--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,3 +1,7 @@
-<script setup></script>
+<script setup>
+import { useCounterStore } from '@/stores/counter'
+//Inicializamos la tienda
+const counterStore = useCounterStore() //Ya podemos interactuar con la tienda
+</script>
 <template></template>
 <style scoped></style>
